29747df77c53607eabb4f8b744d99b116fc79f5e
Vijay A. Saraswat <vsaraswa@us.ibm.com>
2016-04-11 14:10:01 -0400

    Avoid duplicate counting of probabilities because of non-determinism
    The two queries below should provide the same probabilistic results:
    
    query(X):- X=a, true(0.5); X=b, true(0.5). % this has two outcomes,
    each with weight 0.5.
    
    and
    
    query(X):- (X=a, true(0.5), (true; true)); (X=b, true(0.5)). % this has
    three outcomes, each with weight 0.5
    
    This is ensured by associating keys with probabilistic choices, and
    then collecting all results for a given set of keys, discarding
    duplicates.  This prevents double-counting, at the cost of having to
    surface keys for probabilistic choices. The user should take care to
    ensure that each probabilistic choice is associated with a different
    key, this can be done by using terms with variables in them as keys.

--- examples/prog1.pl
+++ examples/prog1.pl
@@ -1,14 +1,14 @@
 %% Here is the object program.
 
-biasedCoin(head) :- true(0.4).
-biasedCoin(tail) :- true(0.6).
+biasedCoin(head) :- true(coin_head,0.4).
+biasedCoin(tail) :- true(coin_tail, 0.6).
 
-urn1(blue):- true(0.7).
-urn1(red) :- true(0.3).
+urn1(blue):- true(urn1b, 0.7).
+urn1(red) :- true(urn1t, 0.3).
 
-urn2(red) :- true(0.2).
-urn2(green):- true(0.3).
-urn2(blue):- true(0.5).
+urn2(red) :- true(urn2r, 0.2).
+urn2(green):- true(urn2g, 0.3).
+urn2(blue):- true(urn2b, 0.5).
 
 system(Coin, C1, C2):- biasedCoin(Coin), urn1(C1), urn2(C2).
 
--- src/pcc.pl
+++ src/pcc.pl
@@ -3,8 +3,12 @@
 :-use_module(library(pairs)), use_module(library(rbtrees)).
 
 % pcc(Ans, Query, Z):- 
-%   on backtracking, iterate through all possible results of Query (as
-%   projected onto Ans), with associated probability Z.
+%  Z = v(Key,P), where Key is a sorted list of keys associated with
+%  probabilistic choices, P is the normalized probability.  Ans is
+%  the list of all solutions to Query obtained for the set of probabilistic
+%  choices specified by Key. On backtracking, all possible tuples
+%  (Ans, Query, Z)  are enumerated.
+% 
 pcc(Ans, Query, Z) :- pcc(Ans, Query, Z, []).
 
 % pcc(Ans, Query, Z, Filter):- 
@@ -14,7 +18,7 @@ pcc(Ans, Query, Z) :- pcc(Ans, Query, Z, []).
 pcc(Ans, Query, Z, Filter) :-
 	pcc_q(Ans, Query, R0),
 	filter(Filter, R0, R1),
-	member(Ans-Z, R1).
+	member(Z-Ans, R1).
 
 % pcc_mp(Ans, Query, Z):- 
 %   return the most probable solution for Query (as
@@ -28,10 +32,10 @@ pcc_mp(Ans, Query, Z):- pcc_mp(Ans, Query, Z, []).
 pcc_mp(Ans, Query, Z, Filter) :-
 	pcc_q(Ans, Query, R0),
 	filter(Filter, R0, R1),	
-	max(R1, _-0, Ans-Z).
+	max(R1, v(_,0)-_, Ans-Z).
 
-max([K-V|Rest], _-M, Result):- V >=M, max(Rest, K-V, Result).
-max([_-V|Rest], O-M, Result):- V < M, max(Rest, O-M, Result).
+max([v(K,V)-L|Rest], v(_,M)-_, Result):- V >=M, max(Rest, v(K,V)-L, Result).
+max([v(_,V)-_|Rest], v(K,M)-L, Result):- V < M, max(Rest, v(K,M)-L, Result).
 max([], X, X).
 
 % pcc_q(Ans, Query, Result):- 
@@ -39,49 +43,62 @@ max([], X, X).
 %   into Ans), with associated probability Z.
 
 pcc_q(Ans, Query, Result) :-
-	bagof(Ans-R, pcc_m(Query, 1, R), A),
+	bagof(R-Ans, pcc_m(Query, v([],1), R), A),
 	normalize(A, Result).
 
 %% filter(Tests, Results, FilteredResults) :-
 %%    FilteredResults contains only those results in Results whose
 %%    key is unifiable with each element in Tests.
 filter([], R, R).
-filter([Test| Tests], R, R2):-
-	filter_e(Test, R, R1),
-	filter(Tests, R1, R2).
+filter([Test| Tests], R, R2):- filter_t(Test, R, R1), filter(Tests, R1, R2).
 
-filter_e(_, [], []).
-filter_e(K, [K1-V|Rest], Rest1):- unifiable(K,K1,_), !,
-	Rest1=[K1-V|Rest2], filter_e(K, Rest, Rest2).
-filter_e(K, [_-_|Rest], Rest1):- filter_e(K, Rest, Rest1).
+filter_t(_, [], []).
+filter_t(Test, [v(K,P)-L|Rest], Rest1):-
+	filter_e(Test,L, L1),
+	((L1=[], Rest1=Rest2); (L1=[_|_], Rest1=[v(K,P)-L1|Rest2])),
+	filter_t(Test, Rest, Rest2).
+
+filter_e(_,[],[]).
+filter_e(Test, [V|Rest], Rest1):- unifiable(Test,V,_), !, 
+	Rest1=[V|Rest2], filter_e(Test, Rest, Rest2).
+filter_e(Test, [_|Rest], Rest1):- filter_e(Test, Rest, Rest1).
 
 normalize(List, Result):-
-	sum(List, 0, V0),
-	divide(List, V0, V1),
+        sortKeys(List,List1),
 	empty_assoc(E),
-	to_assoc_plus(V1, E, Assoc),
-	assoc_to_list(Assoc, Result).
+	to_assoc(List1, E, Assoc),
+	assoc_to_list(Assoc, V1),
+	sum(V1, 0, Sum),
+	divide(V1, Sum, V2),
+	% do this only after collecting results via assoc
+	sortValues(V2,Result).
+
+sortValues([], []).
+sortValues([v(K,P)-L|R], [v(K,P)-L1|R1]):- sort(L, L1),  sortValues(R,R1).
+
+sortKeys([], []).
+sortKeys([v(K,P)-L|R], [v(K1,P)-L|R1]):- sort(K, K1),  sortKeys(R,R1).
 
 sum([],  Z, Z).
-sum([_-R|X],Y, Z):- Y1 is Y+R, sum(X,Y1,Z).
+sum([v(_,P)-_|X],Y, Z):- Y1 is Y+P, sum(X,Y1,Z).
 
 divide([], _, []).
-divide([X-R|Y], V, [X-R1|Y1]):- R1 is R/V, divide(Y, V, Y1).
+divide([v(K,P)-L|Y], V, [v(K,P1)-L|Y1]):- P1 is P/V, divide(Y, V, Y1).
 
-to_assoc_plus([], Assoc, Assoc).
-to_assoc_plus([K-V|Rest], Assoc_i, Assoc_o):-
-	get_or_0(K, Assoc_i, V_i),
-	NewV is V+V_i,
-	put_assoc(K, Assoc_i, NewV, Assoc_m),
-	to_assoc_plus(Rest, Assoc_m, Assoc_o).
-get_or_0(K, Assoc, V):- get_assoc(K, Assoc, V1), !, V1=V.
-get_or_0(_, _, 0).
+to_assoc([], Assoc, Assoc).
+to_assoc([K-L|Rest], Assoc_i, Assoc_o):-
+	get_or_default(K, Assoc_i, [], L_i),
+	put_assoc(K, Assoc_i, [L|L_i], Assoc_m),
+	to_assoc(Rest, Assoc_m, Assoc_o).
+get_or_default(K, Assoc, _, V):- get_assoc(K, Assoc, V1), !, V1=V.
+get_or_default(_, _, Def, Def).
 
 pcc_m((G1,G2), I, O):- !, pcc_m(G1, I, M), pcc_m(G2, M, O).
-pcc_m((G1;G2), I, O):- !, pcc_m(G1, I, O); pcc_m(G2, I, O).
+pcc_m((G1;G2), I, O):- !, (pcc_m(G1, I, O); pcc_m(G2, I, O)).
 pcc_m(true,    I, O):- !, O=I.
-pcc_m(true(V), I, O):- !, O is I*V.
-pcc_m(X\==Y,   I, O):- !, X \==Y, I=O.
+pcc_m(true(K, P), v(L, N), O):- !, N1 is N*P, O=v([K|L], N1).
+pcc_m(X\==Y,   I, O):- !, X \==Y, O=I.
+pcc_m(X=Y,     I, O):- !, X=Y, O=I.
 pcc_m(G,       I, O):- clause(G, Body), pcc_m(Body, I, O).
 
 
